98719fc0832836b7cfe38ebdae3c8d5c3a639944
anhquan99 <anhquanala@gmail.com>
2024-06-02 13:37:44 +0700

    update Heap

---
+++ DataStructure/MinHeap.cs
@@ -0,0 +1,75 @@
+namespace Application;
+public class MinHeap
+{
+    List<int> _arr;
+    int _heapSize;
+    public MinHeap()
+    {
+        _arr = new List<int>();
+        _heapSize = 0;
+    }
+    int LeftChild(int n) => 2 * n + 1;
+    int RightChild(int n) => 2 * n + 2;
+    int Parent(int n) => (n - 1) / 2;
+    bool HasLeftChild(int n) => LeftChild(n) < _heapSize;
+    bool HasRightChild(int n) => RightChild(n) < _heapSize;
+    bool HasParent(int n) => Parent(n) >= 0;
+    void Swap(int a, int b)
+    {
+        var temp = _arr[a];
+        _arr[a] = _arr[b];
+        _arr[b] = temp;
+    }
+    void HeapifyDown()
+    {
+        var index = 0;
+        while (HasLeftChild(index))
+        {
+            var smallest = LeftChild(index);
+            if (HasRightChild(index) && _arr[LeftChild(index)] > _arr[RightChild(index)])
+            {
+                smallest = RightChild(index);
+            }
+            if (_arr[index] < _arr[smallest])
+            {
+                break;
+            }
+            else
+            {
+                Swap(index, smallest);
+            }
+            index = smallest;
+        }
+    }
+    void HeapifyUp()
+    {
+        var index = _heapSize - 1;
+        while (HasParent(index) && _arr[index] < _arr[Parent(index)])
+        {
+            Swap(index, Parent(index));
+            index = Parent(index);
+        }
+    }
+    public void Add(int value)
+    {
+        _arr.Add(value);
+        _heapSize++;
+        if (_heapSize == 1) return;
+        HeapifyUp();
+    }
+    public int Delete()
+    {
+        var result = _arr[0];
+        _arr[0] = _arr[_heapSize - 1];
+        _heapSize--;
+        HeapifyDown();
+        _arr.RemoveAt(_arr.Count - 1);
+        return result;
+    }
+    public int Peek()
+    {
+        if (_heapSize == 0) throw new InvalidOperationException("Heap is empty!");
+        return _arr[0];
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,23 +1,27 @@
 ﻿using Application;
 
-MaxHeap heap = new MaxHeap(15);
+var heap = new MinHeap();
 int k, i, n = 6;
 Console.WriteLine("Entered 6 keys:- 3, 10, 12, 8, 2, 14");
 
-heap.InsertKey(3);
-heap.InsertKey(10);
-heap.InsertKey(12);
-heap.InsertKey(8);
-heap.InsertKey(2);
-heap.InsertKey(14);
+heap.Add(3);
+heap.Add(10);
+heap.Add(12);
+heap.Add(8);
+heap.Add(2);
+heap.Add(14);
 
-Console.WriteLine($"The current size of the heap is {heap.CurrentSize()}");
-Console.WriteLine($"The current maximum element is {heap.GetMax()}");
+Console.WriteLine($"The current max element is {heap.Peek()}");
 
-heap.DeleteKey(2);
-Console.WriteLine($"The current size of the heap is {heap.CurrentSize()}");
-
-heap.InsertKey(15);
-heap.InsertKey(5);
-Console.WriteLine($"The current size of the heap is {heap.CurrentSize()}");
-Console.WriteLine($"The current maximum element is {heap.GetMax()}");
+heap.Delete();
+Console.WriteLine($"The current max element is {heap.Peek()}");
+heap.Delete();
+Console.WriteLine($"The current max element is {heap.Peek()}");
+heap.Delete();
+Console.WriteLine($"The current max element is {heap.Peek()}");
+heap.Delete();
+Console.WriteLine($"The current max element is {heap.Peek()}");
+heap.Delete();
+Console.WriteLine($"The current max element is {heap.Peek()}");
+// heap.Delete();
+// Console.WriteLine($"The current max element is {heap.Peek()}");
